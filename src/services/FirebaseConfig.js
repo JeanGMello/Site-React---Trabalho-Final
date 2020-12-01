c54da37f8ff36dc56dfd12660693a262afe155cc
JeanGMello <71108435+JeanGMello@users.noreply.github.com>
2020-11-30 22:14:51 -0300

    firebase adicionado!

---
+++ src/services/FirebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5dN4C96_23SsaZsRxdK0yGU7O5SOWAaA",
+    authDomain: "thelastfandom-7b9a4.firebaseapp.com",
+    databaseURL: "https://thelastfandom-7b9a4.firebaseio.com",
+    projectId: "thelastfandom-7b9a4",
+    storageBucket: "thelastfandom-7b9a4.appspot.com",
+    messagingSenderId: "691293280267",
+    appId: "1:691293280267:web:4ab0bac65e8c7ba2ddcf0e"
+  };
+
+  export default firebaseConfig;
